bf3b0c89facf04440f26e7236311dd5ac5f40cab
bigcoqui <102246267+bigcoqui@users.noreply.github.com>
2022-05-15 08:10:07 -0300

    update Main.hx

--- source/Main.hx
+++ source/Main.hx
@@ -32,6 +32,8 @@ class Main extends Sprite
 	public function new()
 	{
 		super();
+		
+		SUtil.gameCrashCheck();
 
 		if (stage != null)
 		{
@@ -67,9 +69,7 @@ class Main extends Sprite
 			gameHeight = Math.ceil(stageHeight / zoom);
 		}
 
-		#if !debug
-		initialState = TitleState;
-		#end
+		SUtil.doTheCheck();
 	
 		ClientPrefs.loadDefaultKeys();
 		// fuck you, persistent caching stays ON during sex
@@ -77,7 +77,7 @@ class Main extends Sprite
 		// the reason for this is we're going to be handling our own cache smartly
 		addChild(new FlxGame(gameWidth, gameHeight, initialState, zoom, framerate, framerate, skipSplash, startFullscreen));
 
-		#if !mobile
+		//#if !mobile
 		fpsVar = new FPS(10, 3, 0xFFFFFF);
 		addChild(fpsVar);
 		Lib.current.stage.align = "tl";
@@ -85,7 +85,7 @@ class Main extends Sprite
 		if(fpsVar != null) {
 			fpsVar.visible = ClientPrefs.showFPS;
 		}
-		#end
+		//#end
 
 		#if html5
 		FlxG.autoPause = false;
