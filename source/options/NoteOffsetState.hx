221a8f782ee752f03545b61059ef34e02c9d0d0d
bigcoqui <102246267+bigcoqui@users.noreply.github.com>
2022-05-15 09:51:23 -0300

    update NoteOffsetState.hx

--- source/options/NoteOffsetState.hx
+++ source/options/NoteOffsetState.hx
@@ -195,6 +195,11 @@ class NoteOffsetState extends MusicBeatState
 
 		Conductor.changeBPM(128.0);
 		FlxG.sound.playMusic(Paths.music('offsetSong'), 1, true);
+		
+		#if android
+		addVirtualPad(FULL, A_B);
+		addPadCamera();
+		#end
 
 		super.create();
 	}
