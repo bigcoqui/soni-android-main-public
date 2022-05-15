791c1e9ca80551cbbfb8b27c34249dbf8714235a
bigcoqui <102246267+bigcoqui@users.noreply.github.com>
2022-05-15 09:49:08 -0300

    update BaseOptionsMenu.hx

--- source/options/BaseOptionsMenu.hx
+++ source/options/BaseOptionsMenu.hx
@@ -123,6 +123,10 @@ class BaseOptionsMenu extends MusicBeatSubstate
 
 		changeSelection();
 		reloadCheckboxes();
+		
+		#if android
+		addVirtualPad(FULL, A_B);
+		#end
 	}
 
 	public function addOption(option:Option) {
